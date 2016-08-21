d65eac545afb9a66de370e32a19eee8a8e1ab1cf
lurenzhong <lurenzhong@xiaomi.com>
2016-08-21 16:48:31 +0800

    新建入口文件index.php并定义常用目录

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author lurenzhong@xiaomi.com
+ * @date 2016-08-21 16:39
+ * @file index.php
+ * @brief 框架入口文件
+ * @version
+ */
+
+//定义常用目录
+define('PHALPHP', realpath('./'));
+define('APP', PHALPHP.'/app');
+define('CORE', PHALPHP.'/core');
+
+define('DEBUG', true);
+
+if( DEBUG ){
+    ini_set('display_errors', 'On');
+}else{
+    ini_set('display_errors', 'Off');
+}
+
+
+
